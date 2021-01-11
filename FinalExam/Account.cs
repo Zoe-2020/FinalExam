fc50c39f5fa0cfdc56c1a480fdbd7525bd1079b6
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-01-11 09:59:29 +0000

    Q1(e)

---
+++ FinalExam/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    abstract class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double Balance { get; set; }
+
+        public double InterestDate { get; set; }
+
+        public virtual double Deposit()
+        {
+            return 1 * 2;
+        }
+
+        public virtual double Withdraw()
+        {
+            return 1 * 2;
+        }
+
+        public abstract double CalculateInterest();
+    }
+    class CurrentAccount : Account
+    {
+        double Interest = 0.03;
+
+        public override double CalculateInterest()
+        {
+            return Balance * Interest;
+    
+        }
+
+
+
+
+    }
+
+    class SavingsAccount : Account
+    {
+        double Interest = 0.06;
+
+        public override double CalculateInterest()
+        {
+            return Balance * Interest;
+
+        }
+
+
+
+    }
+}
